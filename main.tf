5a77f0a4223adfb6056da204969f7598b3bb62ab
volkan <vlkn38akyz@gmail.com>
2022-08-16 16:33:07 +0200

    version-4

--- main.tf
+++ main.tf
@@ -53,16 +53,16 @@ resource "aws_security_group" "tf-sec-gr" {
   tags = {
     Name = var.tag
   }
-
   dynamic "ingress" {
-    for_each = var.terraform-instance-ports
+    for_each = var.docker-instance-ports
     iterator = port
     content {
-      from_port   = port.value
-      to_port     = port_value
-      protocol    = "tcp"
+      from_port = port.value
+      to_port = port.value
+      protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
+  
   }
   egress {
     from_port   = 0
